22083e977cd7e5059ba048d8c1cc8dac450cd108
Stefania Formato <sformato95@gmail.com>
2026-04-13 23:57:28 +0200

    404 and http states

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';;
+import express from 'express';
 
 const app = express();
 
@@ -6,6 +6,14 @@ app.get('/', (req, res)  => {
     console.log(req.url);
     console.log(req.headers);
     return res.send("<h1>Ciao Lacerba</h1> <p> Questo è il nostro primo server in NodeJS! </p>");
+});
+
+app.get('/ciao', (req, res)  => {
+    return res.send("<h1>Ciao Lacerba</h1> <p> Questa è la pagina ciao! </p>");
+});
+
+app.get('*splat', (req, res)  => {
+    return res.status(404).send("<h1> 404 </h1> <p> Pagina non trovata! </p>");
 });
 
 app.listen(3000, () => {
