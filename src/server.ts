57ecf71117f11e7040223fc1f38690db7956064f
Stefania Formato <sformato95@gmail.com>
2026-04-14 00:27:35 +0200

    Dinamic pages and url

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,19 @@ app.get('/', (req, res)  => {
     return res.send("<h1>Ciao Lacerba</h1> <p> Questo è il nostro primo server in NodeJS! </p>");
 });
 
-app.get('/ciao', (req, res)  => {
-    return res.send("<h1>Ciao Lacerba</h1> <p> Questa è la pagina ciao! </p>");
+app.get('/greeting/:name', (req, res) => {
+    console.log(req.params);
+    const name = req.params.name;
+    return res.send(`<h1>Ciao ${name}</h1> <p> Questo è la pagina greeting! </p>`);
 });
 
-app.get('*splat', (req, res)  => {
-    return res.status(404).send("<h1> 404 </h1> <p> Pagina non trovata! </p>");
+app.get('/ciao', (req, res) => {
+    return res.send("<h1>Ciao Lacerba</h1> <p> Questo è la pagina ciao! </p>");
 });
+
+app.get('*splat', (req, res) => {
+    return res.status(404).send("<h1>404 Not Found</h1> <p> La pagina richiesta non esiste! </p>");
+}) 
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
