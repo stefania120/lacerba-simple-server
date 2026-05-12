f3984542bbaf9f174499e0eabc8b07b0e2c22b25
Stefania Formato <sformato95@gmail.com>
2026-05-12 02:31:17 +0200

    post and body request

--- src/api.ts
+++ src/api.ts
@@ -2,10 +2,23 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 let cnt = 0;
 app.get('/', (req, res) => {
     cnt += 1;
     res.send({hello: 'world', cnt});
+});
+
+app.post('/', (req, res) => {
+    cnt += -1;
+    res.send({hello: 'world', cnt});
+});
+
+app.post('/reply', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    res.send({body:body});
 });
 
 const PORT = 3000;
