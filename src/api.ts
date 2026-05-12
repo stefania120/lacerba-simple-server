d138eb12313f5cd38c23c78237b35b7f799df07f
Stefania Formato <sformato95@gmail.com>
2026-05-12 18:34:26 +0200

    API CRUD and CRUD Read

--- src/api.ts
+++ src/api.ts
@@ -5,21 +5,62 @@ const app = express();
 app.use(express.json());
 
 let cnt = 0;
-app.get('/', (req, res) => {
-    cnt += 1;
-    res.send({hello: 'world', cnt});
+
+// CRUD - Create, Read, Update, Delete
+
+interface BlogPost {
+    title: string;
+    date: Date;
+    body: string;
+    id: number;
+    draft: boolean;
+}
+
+const posts: BlogPost[] = [{
+    id:0,
+    title: 'My first blog post',
+    date: new Date(),
+    body: 'This is the content of my first blog post.',
+    draft: false
+},
+{
+    id:1,
+    title: 'My second blog post',
+    date: new Date(),
+    body: 'This is the content of my second blog post.',
+    draft: false
+}
+];
+
+app.get ('/posts/', (req, res) => {
+    res.send(posts);
 });
 
-app.post('/', (req, res) => {
-    cnt += -1;
-    res.send({hello: 'world', cnt});
+app.get ('/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    if (!post) {
+       return res.status(404).send({error: 'Post not found'});
+        
+    }
+    return res.send({post});
 });
 
-app.post('/reply', (req, res) => {
-    const body = req.body;
-    console.log(body);
-    res.send({body:body});
-});
+// app.get('/', (req, res) => {
+//     cnt += 1;
+//     res.send({hello: 'world', cnt});
+// });
+
+// app.post('/', (req, res) => {
+//     cnt += -1;
+//     res.send({hello: 'world', cnt});
+// });
+
+// app.post('/reply', (req, res) => {
+//     const body = req.body;
+//     console.log(body);
+//     res.send({body:body});
+// });
 
 const PORT = 3000;
 app.listen(PORT, () => {
